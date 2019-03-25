fix acc calculation

evaluate.py
@@ -27,8 +27,9 @@ def evaluate(model, validation_loader, summary_writer=None, global_step=None, n_
     # TODO: Compute accuracy
     queries = []
     preds = []
+    corrects = []
 
-    n_correct = 0
+    n_correct = 0.
     criterion = torch.nn.NLLLoss(reduction='sum')
     val_loss = 0
 
@@ -51,7 +52,12 @@ def evaluate(model, validation_loader, summary_writer=None, global_step=None, n_
             # TODO: Log the probabilities as well
 
             pred = topi.squeeze(1)
-            n_correct += sum(pred == y)
+            for i, query in enumerate(b_queries):
+                if pred[i] in query['gt_ppos_all']:
+                    n_correct += 1
+                    corrects.append(1)
+                else:
+                    corrects.append(0)
 
             # print(torch.log(attn_weights[0]))
             # print(y[0])
@@ -62,19 +68,19 @@ def evaluate(model, validation_loader, summary_writer=None, global_step=None, n_
             preds.extend(pred.cpu().numpy())
 
     n_queries = len(validation_loader.dataset)
-    acc = n_correct.float()/n_queries
+    acc = n_correct/n_queries
     val_loss = val_loss/n_queries
 
     # Drawing samples
-    sample_queries, sample_preds = zip(*random.sample(list(zip(queries, preds)), n_samples))
+    sample_queries, sample_preds, sample_corrects = zip(*random.sample(list(zip(queries, preds, corrects)), n_samples))
 
     loader = transforms.ToTensor()
-    for (query, pred) in zip(sample_queries, sample_preds):
+    for (query, pred, correct) in zip(sample_queries, sample_preds, sample_corrects):
         image_id = query['image_id']
         proposal_bboxes = validation_loader.dataset.proposals[image_id]
         filename = os.path.join(IMG_RAW_DIR, image_id+'.jpg')
 
-        image = misc.inference_image(filename, query['gt_boxes'], [proposal_bboxes[query['gt_ppos_id']]], [proposal_bboxes[pred]], ' '.join(query['phrase']))
+        image = misc.inference_image(filename, query['gt_boxes'], [proposal_bboxes[query['gt_ppos_id']]], [proposal_bboxes[pred]], correct, ' '.join(query['phrase']))
 
         # Saving
         if not global_step:

train.py
@@ -106,7 +106,13 @@ def train():
             attn_weights = grounder(b_pr_features, (lstm_h0, lstm_c0), b_ph_indices, batch_size)
 
             topv, topi = attn_weights.topk(1)
-            acc = sum(topi.squeeze(1) == b_y).float()/batch_size
+            train_acc = 0.
+            pred = topi.squeeze(1)
+            for i, query in enumerate(b_queries):
+                if pred[i] in query['gt_ppos_all']:
+                    train_acc += 1
+
+            train_acc = train_acc/batch_size
 
             loss = criterion(torch.log(attn_weights), b_y)
 
@@ -116,14 +122,14 @@ def train():
             optimizer.zero_grad()
 
             running_loss += loss 
-            running_acc += acc
+            running_acc += train_acc
             global_step = epoch*len(train_loader)+batch_idx
 
             # Log losses
             print_batches = PRINT_EVERY//BATCH_SIZE
             if batch_idx % print_batches == print_batches-1:
                 writer.add_scalar('loss', loss.item(), global_step)
-                writer.add_scalar('train_acc', acc.item(), global_step)
+                writer.add_scalar('train_acc', train_acc, global_step)
                 logging.info("Epoch %d, query %d, loss: %.3f, acc: %.3f" % (epoch+1, (batch_idx+1)*BATCH_SIZE, running_loss/print_batches, running_acc/print_batches))
                 running_loss = 0
                 running_acc = 0